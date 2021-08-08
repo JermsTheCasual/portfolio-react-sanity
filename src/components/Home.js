61ed957d22bcecd5cd03f02a01f556843e0772df
Jeremy Taylor <jeremy.taylor92@gmail.com>
2021-08-08 09:03:23 -0400

    fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,8 @@ export default function Home() {
         <main>
             <img 
               className="absolute object-cover w-full h-full" 
-            />
+              alt="background-img"
+                />
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
                 <h1 className="tex-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Hello. I'm Jeremy.</h1>
             </section>
